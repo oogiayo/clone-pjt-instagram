667a4dffa24d3c71ed6a09ef7be7a51c34d89230
oogiayo <donguk.smile@gmail.com>
2020-10-01 18:55:24 +0900

    feat: Add User Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, require_GET, require_http_methods
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 
 # Create your views here.
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method=='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.method=='POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            next_url = request.GET.get('next')
+            return redirect(next_url or 'posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return redirect('posts:index')
+    
+    return redirect('accounts:login')
+
+
+@require_http_methods(['GET', 'POST'])
+def update(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:login')
+
+    if request.method=='POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def delete(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:login')
+    
+    if request.method=='POST':
+        request.user.delete()
+        return redirect('posts:index')
+    
+    return render(request, 'accounts/delete.html')
+    
+
+
+@require_http_methods(['GET', 'POST'])
+def password_update(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:login')
+
+    if request.method=='POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            auth_login(request, request.user)
+            return redirect('accounts:update')
+    else:       
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password_update.html', context)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
-    pass
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
